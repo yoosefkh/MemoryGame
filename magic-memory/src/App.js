7910bb573ef11e1758bf76ce2298ceecd2ccf2b9
yoosefkh <yoosefkh@gmail.com>
2023-02-20 16:25:34 +0200

    creating card grid & components + mkaing card choices

---
+++ magic-memory/src/App.js
@@ -0,0 +1,46 @@
+
+import { useState } from 'react';
+import './App.css';
+import SingleCard from "./components/SingleCard"
+
+
+const cardImages = [
+  { src: "/img/helmet-1.png" },
+  { src: "/img/potion-1.png" },
+  { src: "/img/ring-1.png" },
+  { src: "/img/scroll-1.png" },
+  { src: "/img/shield-1.png" },
+  { src: "/img/sword-1.png" }
+];
+
+function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+
+
+//shuffle cards
+  
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map( card => ({...card, id: Math.random() }))
+    
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+
+  return (
+    <div className="App">
+      <h1> Magic Match</h1>
+      <button onClick = {shuffleCards}> New Game</button>
+
+      <div className="card-grid">
+        {cards.map( card => (
+          <SingleCard key={card.id} card = {card} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
